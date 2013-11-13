d218c226ff16b6985f404358eb9371611a03754c
gustavoferreira0701 <gustavoferreira0701@hotmail.com>
2013-11-12 23:37:00 -0200

    Adicionando arquivos ao repositório

---
+++ AjaxExample/Pagina.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Pagina.aspx.cs" Inherits="AjaxExample.Pagina" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <!-- Na página aspx é necessário adicionar a tag scriptmanager e habilitar o atributo EnablePageMethods-->
+        <asp:ScriptManager ID="scmManager" runat="server" EnablePageMethods="true"></asp:ScriptManager>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ AjaxExample/Pagina.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AjaxExample
+{
+    public partial class Pagina : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Para que a requisição ajax encontre o método é necessário 
+        /// "decorá-lo" com o atributo webmethod, que o transforma em um webservice, 
+        /// a partir daí e possível que seja feita a comunicação via ajax.
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="telefone"></param>
+        /// <param name="endereco"></param>
+        /// <param name="idade"></param>
+        [WebMethod]
+        public static void MetodoSimples(string nome, string telefone, string endereco, int idade)
+        {
+            try
+            {
+                Pessoa p = new Pessoa { Nome = nome, Telefone = telefone, Endereco = endereco, Idade = idade };
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+    }
+
+
+    public class Pessoa
+    {
+        
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public int Idade { get; set; }
+    }
+}
